1f630141eb4f6e1fb2f33e96c2d5442a9461af27
Reza <rzgholizadeh@gmail.com>
2020-04-14 19:44:46 -0600

    chore: Add the definition of process as comments

--- src/main/java/nrg/sdnsimulator/web/services/StudyService.java
+++ src/main/java/nrg/sdnsimulator/web/services/StudyService.java
@@ -8,7 +8,9 @@ import com.fasterxml.jackson.databind.util.JSONPObject;
 public class StudyService {
 
 	public JSONPObject runStudy(JSONPObject config) {
-
+		// uses topology and network service to create the set of simulations to be run
+		// uses the simulation service to run the simulation and get the results
+		// the results can be either singular or the whole (yet to be decided)
 		return null;
 	}
 
